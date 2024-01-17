db8e5a607ba487857b045633d70aaf00e6d57fbd
Mariklimova <mariku22@yandex.ru>
2024-01-17 23:09:02 +0300

    addition buildResponse

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const { createUser, getAllUser, getUserById, updateUser, deleteUser } = require('../service/user.service');
 const { IsValidUserId, IsValidUser } = require('../helper/validation');
+const { buildResponse } = require('express')
+
 const route = express.Router();
 
 route.post('/', IsValidUser, async (req, res) => {
   try {
     const { name, surname, birth, city, age } = req.body;
     const data = await createUser(name, surname, birth, city, age);
-    res.status(200).send(data);
+    buildResponse(200, data, res);
   } catch (error) {
-    res.status(404).send(error.message);
+    buildResponse(404, error.message, res);
   }
 });
 
 route.get('/', async (_req, res) => {
   try {
     const data = await getAllUser();
-    res.status(200).send(data);
+    buildResponse(200, data, res);
   } catch (error) {
-    res.status(404).send(error.message);
+    buildResponse(404, error.message, res);
   }
 });
 
@@ -26,9 +28,9 @@ route.get('/:id', IsValidUserId, async (req, res) => {
   try {
     const { id } = req.params;
     const data = await getUserById(id);
-    res.status(200).send(data);
+    buildResponse(200, data, res);
   } catch (error) {
-    res.status(404).send(error.message);
+    buildResponse(404, error.message, res);
   }
 });
 
@@ -37,9 +39,9 @@ route.put('/:id/:info_id', IsValidUserId, IsValidUser, async (req, res) => {
     const { id, info_id } = req.params;
     const { name, surname, birth, city, age } = req.body;
     const data = await updateUser(id, name, surname, birth, city, age, info_id);
-    res.status(200).send(data);
+    buildResponse(200, data, res);
   } catch (error) {
-    res.status(404).send(error.message);
+    buildResponse(404, error.message, res);
   }
 });
 
@@ -47,9 +49,9 @@ route.delete('/:id', IsValidUserId, async (req, res) => {
   try {
     const { id } = req.params;
     const data = await deleteUser(id);
-    res.status(200).send(data);
+    buildResponse(200, data, res);
   } catch (error) {
-    res.status(404).send(error.message);
+    buildResponse(404, error.message, res);
   }
 });
 
